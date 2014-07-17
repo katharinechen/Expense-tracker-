6ccf4dac2e8d492552a96fbcd2cb670345e7b1c3
Katharine Chen <katharinechen.ny@gmail.com>
2014-07-17 09:36:01 -0700

    Git update

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+var Categories = { 
+  products: [] 
+}; 
+
 var Product = {
 
   totalCost: function() { 
@@ -9,39 +13,68 @@ var Product = {
 };
 
 
+
 $(document).ready(function() {
 
-  var Categories = { name: name, products: []}; 
+  var CategoriesArray = []; 
 
-  $("#add-purchases").submit(function(event) {
-    Product.descriptionProduct = $("#description").val();
-    Product.quantity = $("#quantity").val(); 
-    Product.amount = $("#amount").val();
+  $("#category-form").submit(function(event) { 
+    var newCategory = Object.create(Categories); 
+    newCategory.nameOf = $("#category").val(); 
+    CategoriesArray.push(newCategory); 
+    event.preventDefault(); 
 
-    $("#tbody-results").append("<tr><td>" + Product.descriptionProduct +
-                               "</td><td>" + Product.quantity + 
-                               "</td><td>" + Product.amount +
-                               "</td><td>" + Product.totalCost() + 
-                               "</td></tr>" );
+    $("#cat-results").append("<li>" + newCategory.nameOf + "</li>"); 
+    $("#category").val(""); 
 
-    var newPurchase = Object.create(Product); 
-    newPurchase.descriptionProduct = Product.descriptionProduct; 
-    newPurchase.quantity = Product.quantity; 
-    newPurchase.amount = Product.amount; 
-    newPurchase.amount = Product.totalCost(); 
+    $("#cat-results").last().click(function() { 
+      $("#add-purchases").show(); 
+      $("#add-purchases h3").text("Add Purchases to" ); 
+      event.preventDefault(); 
+    }); 
 
-    console.log(newPurchase);  
-
-    $("#description").val("");
-    $("#quantity").val(""); 
-    $("#amount").val("");
-
-    event.preventDefault();
-
-  });
+  }); 
 
 
+
+    // $(".contact").last().click(function() {
+    //   $("#show-contact").show();
+
+    //   $("#show-contact h2").text(newContact.firstName);
+    //   $(".first-name").text(newContact.firstName);
+    //   $(".last-name").text(newContact.lastName);
+
+    //   $("ul#addresses").text("");
+    //   newContact.addresses.forEach(function(address) {
+    //     $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+    //   });
+    // });
+
+  
+
+  // $("#add-purchases").submit(function(event) {
+  //   Product.descriptionProduct = $("#description").val();
+  //   Product.quantity = $("#quantity").val(); 
+  //   Product.amount = $("#amount").val();
+
+  //   $("#tbody-results").append("<tr><td>" + Product.descriptionProduct +
+  //                              "</td><td>" + Product.quantity + 
+  //                              "</td><td>" + Product.amount +
+  //                              "</td><td>" + Product.totalCost() + 
+  //                              "</td></tr>" );
+
+  //   var newPurchase = Object.create(Product); 
+  //   newPurchase.descriptionProduct = Product.descriptionProduct; 
+  //   newPurchase.quantity = Product.quantity; 
+  //   newPurchase.amount = Product.amount; 
+  //   newPurchase.amount = Product.totalCost(); 
+
+  //   console.log(newPurchase);  
+
+  //   $("#description").val("");
+  //   $("#quantity").val(""); 
+  //   $("#amount").val("");
+
+  //   event.preventDefault();
+  // });
 });
-
-
-// var yourBankAccount = Object.create(BankAccount);
