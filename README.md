96ab90ba2a18c0015d323e1e9ed9fc3a6e8a48bd
Katharine Chen <katharinechen.ny@gmail.com>
2014-09-23 13:07:48 -0700

    Fix README.md

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
-<h1>Expense Tracker</h1>
+#Expense Tracker 
 
-<b>Step 1:</b> Make a web page to track your expenses. To start off with, just make a form where users can input a description and amount for each purchase, and list out all the purchases:
+##About 
+This project is completed by Katharine Chen and [Austin Steward](https://github.com/asteward) as part of the [Epicodus program](http://www.learnhowtoprogram.com/lessons/expense-tracker). 
 
+##Description 
+The goal of this project is to create a web page to track your expenses written in Javascript. 
 
-Add a quantity property, and a totalCost() method.Make a prototype called Purchase to create objects for the purchases. It's always a good idea to pick out names for prototypes that closely match the real-world language we use to talk about them.
+##Functionality 
+* Users can add categories 
+* Users can add different items to different categories 
 
-An expense tracker isn't very useful if you can't categorize your purchases. Make a form where users can create new categories. When users picks a category, it lists all the purchases in the category, and has a place to add new purchases: 
+##Installation 
+Clone this repository in your terminal. 
+
+    git clone https://github.com/katharinechen/Expense-tracker-.git
+
+Open scripts.js in your browser. 
+
+    scripts.js 
+
+##License
+MIT 
--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ var Categories = {
     this.nameOf = categoryName;
     this.products = [];
   },
-
   pushFunction: function(purchaseItem) {
     products.push(purchaseItem);
   },
@@ -14,15 +13,12 @@ var Categories = {
 };
 
 var Product = {
-
   totalCost: function() {
     var total = 0;
     var total = this.quantity * this.amount;
     return total;
   }
-
 };
-
 
 $(document).ready(function() {
 
@@ -63,10 +59,6 @@ $(document).ready(function() {
     newPurchase.amount = $("#amount").val();
     newPurchase.total = newPurchase.totalCost();
 
-
-
-
-
     $("#tbody-results").append("<tr><td>" +  newPurchase.descriptionProduct +
                                "</td><td>" + newPurchase.quantity +
                                "</td><td>" + newPurchase.amount +
@@ -83,27 +75,3 @@ $(document).ready(function() {
     event.preventDefault();
   });
 });
-
-
-
-
-
-
-
-
-
-    // Product.descriptionProduct = $("#description").val();
-    // Product.quantity = $("#quantity").val();
-    // Product.amount = $("#amount").val();
-
-    // $("#tbody-results").append("<tr><td>" + Product.descriptionProduct +
-    //                            "</td><td>" + Product.quantity +
-    //                            "</td><td>" + Product.amount +
-    //                            "</td><td>" + Product.totalCost() +
-    //                            "</td></tr>" );
-
-    // var newPurchase = Object.create(Product);
-    // newPurchase.descriptionProduct = Product.descriptionProduct;
-    // newPurchase.quantity = Product.quantity;
-    // newPurchase.amount = Product.amount;
-    // newPurchase.amount = Product.totalCost();
